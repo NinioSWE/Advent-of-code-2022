caad31140a7c7d659b9e2673bc18b0d190fcb8d4
Per Eriksson <Pereriksson98@hotmail.com>
2022-12-01 20:19:11 +0100

    day1

---
+++ Day1/main.rb
@@ -0,0 +1,17 @@
+file = open("input.txt")
+tempNum = 0
+highest = 0
+sumList = Array.new 
+file.each_line { |line|
+    tempNum += line.to_i
+    if line == "\n"
+        sumList << tempNum
+        tempNum = 0
+    end
+}
+
+sortedSumList = sumList.sort
+puts "first answer: #{sortedSumList.last}"
+puts "second answer: #{sortedSumList.last(3).sum}"
+
+file.close
